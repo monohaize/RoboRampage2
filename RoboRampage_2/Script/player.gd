e547a798d70bcced4615ec79f5c0a07c5a99c40c
monohaize <monohaize@gmail.com>
2025-08-22 12:14:22 +1200

    2nd commit

--- RoboRampage_2/Script/player.gd
+++ RoboRampage_2/Script/player.gd
@@ -2,17 +2,24 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var jump_height: float = 4.0
+var mouse_motion := Vector2.ZERO
+@onready var camera_pivot: Node3D = $CameraPivot
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(delta: float) -> void:
+	handle_camera_rotation()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y -= gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 4.0 * gravity)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -24,5 +31,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			mouse_motion = -event.relative * 0.001
+		if event.is_action_pressed("ui_cancel"):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func handle_camera_rotation() -> void:
+	rotate_y(mouse_motion.x)
+	camera_pivot.rotation_degrees.x = clampf(
+		camera_pivot.rotation_degrees.x, -90.0, 90.0
+	)
+	camera_pivot.rotate_x(mouse_motion.y) 
+	mouse_motion = Vector2.ZERO
 
 	move_and_slide()
---
+++ RoboRampage_2/control.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _draw() -> void:
+	draw_circle(Vector2.ZERO, 4, Color.BLACK)
+	
+	draw_line(Vector2(16,0), Vector2(24,0), Color.BLACK )
+	draw_line(Vector2(-16,0), Vector2(-24,0), Color.BLACK )
+	
+	draw_line(Vector2(0,16), Vector2(0,24), Color.BLACK )
+	draw_line(Vector2(0,-16), Vector2(0,-24), Color.BLACK )
